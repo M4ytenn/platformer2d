20e10e96d17aa8f77204c77b4496bae87df38cba
M4ytenn <mike_pro@internet.ru>
2024-05-24 18:18:34 +0300

    1

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -5,13 +5,4 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-    [SerializeField] private PlayerMover _mover;
-    [SerializeField] private PlayerCombat _combat;
-
-    private void Update()
-    {
-        _animator.SetFloat("VelocityX", _mover.Rigidbody.velocity.x);
-        _animator.SetFloat("VelocityY", _mover.Rigidbody.velocity.y);
-        _animator.SetBool("IsAttacking", _combat.IsAttacking);
-    }
 }
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerController : MonoBehaviour
+{
+    
+}
+
+// Zenject, New Input System, State Machine, rework Animator
